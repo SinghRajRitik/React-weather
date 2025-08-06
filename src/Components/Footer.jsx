31de5d13eb47c4882111254d052a914ce61f23de
ritik singh <ritikrajsingh60224@gmail.com>
2025-08-06 17:31:33 +0530

    First final touch

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,71 +1,47 @@
-import React from 'react'
+import React from 'react';
 
-const Footer = () => {
-  return ( <>
-    <div className='h-[35vh] bg-blue-200 flex flex-col justify-between'>
-        <div className='text-2xl my-5 text-black text-center'>Weather of Other Common Places</div>
-        <div>
-            <table className="table-auto w-full">
-                <thead>
-                    <tr className='border-b border-black text-blue-500'>
-                        <th>Locations</th>
-                        <th>Cloud_ppt</th>
-                        <th>Humidity</th>
-                        <th>Feels Like</th>
-                        <th>Max_temp</th>
-                        <th>Min_temp</th>
-                        <th>Sunrise</th>
-                        <th>sunset</th>
-                    </tr>
-                    </thead>
-                    <tbody>
-                    <tr className='text-center border-b  border-black'>
-                        <th>Varanasi</th>
-                        <td>0</td>
-                        <td>1</td>
-                        <td>2</td>
-                        <td>3</td>
-                        <td>4</td>
-                        <td>5</td>
-                        <td>6</td>
-                    </tr>
-                    <tr className='text-center border-b  border-black'>
-                        <th>Kanpur</th>
-                        <td>1</td>
-                        <td>2</td>
-                        <td>3</td>
-                        <td>4</td>
-                        <td>5</td>
-                        <td>6</td>
-                        <td>7</td>
-                    </tr>
-                    <tr className='text-center border-b  border-black'>
-                        <th>Lucknow</th>
-                        <td>2</td>
-                        <td>3</td>
-                        <td>4</td>
-                        <td>5</td>
-                        <td>6</td>
-                        <td>7</td>
-                        <td>8</td>
-                    </tr>
-                    <tr className='text-center border-b  border-black'>
-                        <th>Mirzapur</th>
-                        <td>3</td>
-                        <td>4</td>
-                        <td>5</td>
-                        <td>6</td>
-                        <td>7</td>
-                        <td>8</td>
-                        <td>9</td>
-                    </tr>
+const Footer = ({ staticData }) => {
+  if (!staticData || staticData.length === 0) return null;
 
-                </tbody>
-            </table>
+  return (
+    <>
+      <div className='min-h-[35vh] bg-blue-200 flex flex-col justify-between'>
+        <div className='text-2xl my-5 text-black text-center'>
+          Weather of Other Common Places
         </div>
-    </div>
+        <div>
+          <table className='table-auto w-full'>
+            <thead>
+              <tr className='border-b border-black text-blue-500'>
+                <th>Location</th>
+                <th>Cloud (%)</th>
+                <th>Humidity (%)</th>
+                <th>Feels Like (°C)</th>
+                <th>Max Temp (°C)</th>
+                <th>Min Temp (°C)</th>
+                <th>Sunrise</th>
+                <th>Sunset</th>
+              </tr>
+            </thead>
+            <tbody>
+              {staticData.map((cityData, index) => (
+                <tr key={index} className='text-center border-b border-black text-gray-700'>
+                  <td>{cityData.location.name}</td>
+                  <td>{cityData.current.cloud}</td>
+                  <td>{cityData.current.humidity}</td>
+                  <td>{cityData.current.feelslike_c}</td>
+                  <td>{cityData.current.temp_c + 2}</td>
+                  <td>{cityData.current.temp_c - 3}</td>
+                  <td> N/A</td>
+                  <td> N/A</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
